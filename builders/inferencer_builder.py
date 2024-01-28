bf9d422fa3e3fedfec43464c58afea80c39aab97
Chestnut <li1928631@163.com>
2024-01-28 17:44:35 +0800

    新增特性，transform添加剪裁

--- builders/inferencer_builder.py
+++ builders/inferencer_builder.py
@@ -11,33 +11,44 @@ class InferencerBuilder(Builder):
             type,
             weight,
             classes,
-            inference_size,
             required_size,
             batch_size,
-            device=None
+            device=None,
+            resize_size=None,
+            crop_size=None,
+            mean=None,
+            std=None
     ) -> Inferencer:
         if type == 'cla' or type == 'classification':
             model = get_cla_model(classes)
-            transform_cls = ClassificationPresetEval
+            transform = ClassificationPresetEval(
+                resize_size=resize_size,
+                crop_size=crop_size,
+                mean=mean,
+                std=std
+            )
             return ClassificationInferencer(
                 model=model,
                 weight=weight,
                 classes=classes,
-                transform_cls=transform_cls,
-                inference_size=inference_size,
+                transform=transform,
                 required_size=required_size,
                 batch_size=batch_size,
                 device=device
             )
         elif type == 'seg' or type == 'segmentation':
             model = get_seg_model(classes)
-            transform_cls = SegmentationPresetEval
+            transform = SegmentationPresetEval(
+                resize_size=resize_size,
+                crop_size=crop_size,
+                mean=mean,
+                std=std
+            )
             return SegmentationInferencer(
                 model=model,
                 weight=weight,
                 classes=classes,
-                transform_cls=transform_cls,
-                inference_size=inference_size,
+                transform=transform,
                 required_size=required_size,
                 batch_size=batch_size,
                 device=device
@@ -51,10 +62,13 @@ class InferencerBuilder(Builder):
             'type': 'seg',
             'weight': self.config.inferencer.segmentation.weight,
             'classes': self.config.inferencer.segmentation.classes,
-            'inference_size': self.config.inferencer.segmentation.inference_size,  # [h, w]
             'required_size': self.config.slicer.segmentation.slice_size[::-1],  # [w, h] -> [h, w]
             'batch_size': self.config.inferencer.segmentation.batch_size,
             'device': self.config.inferencer.segmentation.device,
+            'resize_size': self.config.inferencer.segmentation.transforms.resize_size,
+            'crop_size': self.config.inferencer.segmentation.transforms.crop_size,
+            'mean': self.config.inferencer.segmentation.transforms.mean,
+            'std': self.config.inferencer.segmentation.transforms.std,
         }
         self.logger.debug(f'seg_inferencer参数-{params_dict}')
         return self.build_from_dict(self.create_inferencer, params_dict)
@@ -65,10 +79,13 @@ class InferencerBuilder(Builder):
             'type': 'cla',
             'weight': self.config.inferencer.classification.weight,
             'classes': self.config.inferencer.classification.classes,
-            'inference_size': self.config.inferencer.classification.inference_size,  # [h, w]
             'required_size': self.config.slicer.classification.slice_size[::-1],  # [w, h] -> [h, w]
             'batch_size': self.config.inferencer.classification.batch_size,
             'device': self.config.inferencer.classification.device,
+            'resize_size': self.config.inferencer.classification.transforms.resize_size,
+            'crop_size': self.config.inferencer.classification.transforms.crop_size,
+            'mean': self.config.inferencer.classification.transforms.mean,
+            'std': self.config.inferencer.classification.transforms.std,
         }
         self.logger.debug(f'cla_inferencer参数-{params_dict}')
         return self.build_from_dict(self.create_inferencer, params_dict)
